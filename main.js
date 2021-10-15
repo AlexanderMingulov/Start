3c82f9af16852c89792d7699021fd9ce4d3e6c79
Alexander <alexandermingulov@gmail.com>
2021-10-15 23:30:41 +0300

    add homework 8

--- main.js
+++ main.js
@@ -5,16 +5,44 @@ const file = '/tmp/file.txt'
 const dir2 = '/tmp2'
 const dirOver = '/tmp2/file.txt'
 
-fs.ensureDir(dir)
-    .then(() => fs.outputFile(file, 'hello!'))
-    .then(() => fs.ensureDir(dir2))
-    .then(() => fs.move(file, dirOver, { overwrite: true }))
-    .then(() => {
-        fs.remove(dir)
-        fs.remove(dir2)
+fs.ensureDir(dir, (err) => {
+    if(err) return console.log(err);
+    fs.outputFile(file, 'hello world!!', (err) => {
+        if(err) return console.log(err);
+        fs.ensureDir(dir2, (err) => {
+            if(err) return console.log(err);
+            fs.move(file, dirOver, { overwrite: true }, (err) => {
+                if(err) return console.log(err);
+                fs.remove(dir, (err) => {
+                    if(err) return console.log(err);
+                    fs.remove(dir2, (err) => {
+                        if(err) {
+                            console.log(err);
+                    
+                        } else { 
+                            console.log('succesfull!')
+                        }
+                    }) 
+                })  
+            })
+        })
     })
-    .then(() => console.log('succsesfull!!!'))
-    .catch((err) => console.log(err))
+})
+
+
+
+
+
+// fs.ensureDir(dir)
+//     .then(() => fs.outputFile(file, 'hello!'))
+//     .then(() => fs.ensureDir(dir2))
+//     .then(() => fs.move(file, dirOver, { overwrite: true }))
+//     .then(() => {
+//         fs.remove(dir)
+//         fs.remove(dir2)
+//     })
+//     .then(() => console.log('succsesfull!!!'))
+//     .catch((err) => console.log(err))
 
 
 
